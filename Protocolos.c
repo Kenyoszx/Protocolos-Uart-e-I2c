729cea5331973e9b16d518a3e98e426bea9bdeb9
João Victor de Souza Silva <joao1452019@gmail.com>
2025-02-03 20:43:21 -0300

    Configuração pio para matriz de leds

--- Protocolos.c
+++ Protocolos.c
@@ -46,6 +46,20 @@ void NUMBER_8();
 void NUMBER_9();
 static void gpio_irq_handler(uint gpio, uint32_t events);
 
+// Definição de pixel GRB
+struct pixel_t
+{
+  uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
+};
+typedef struct pixel_t npLED_t;
+
+// buffer de pixels que formam a matriz.
+npLED_t leds[LED_COUNT];
+
+// Variáveis para uso da máquina PIO.
+PIO np_pio;
+uint sm;
+
 int main()
 {
     stdio_init_all();
@@ -99,4 +113,58 @@ void init()
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+}
+void npInit(uint pin)
+{
+  // Inicializa a máquina PIO para controle da matriz de LEDs.
+
+  // Cria programa PIO.
+  uint offset = pio_add_program(pio0, &ws2818b_program);
+  np_pio = pio0;
+
+  // Toma posse de uma máquina PIO.
+  sm = pio_claim_unused_sm(np_pio, false);
+  if (sm < 0)
+  {
+    np_pio = pio1;
+    sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
+  }
+
+  // Inicia programa na máquina PIO obtida.
+  ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+  // Limpa buffer de pixels.
+  for (uint i = 0; i < LED_COUNT; ++i)
+  {
+    leds[i].R = 0;
+    leds[i].G = 0;
+    leds[i].B = 0;
+  }
+}
+void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b)
+{
+  // Atribui uma cor RGB a um LED.
+
+  leds[index].R = r;
+  leds[index].G = g;
+  leds[index].B = b;
+}
+void npClear()
+{
+  // Limpa o buffer de pixels.
+
+  for (uint i = 0; i < LED_COUNT; ++i)
+    npSetLED(i, 0, 0, 0);
+}
+void npWrite()
+{
+  // Escreve os dados do buffer nos LEDs.
+
+  // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
+  for (uint i = 0; i < LED_COUNT; ++i)
+  {
+    pio_sm_put_blocking(np_pio, sm, leds[i].G);
+    pio_sm_put_blocking(np_pio, sm, leds[i].R);
+    pio_sm_put_blocking(np_pio, sm, leds[i].B);
+  }
 }
